8f52c77c6c820c8940bfa8aab7c83bad37c1532b
Nhat499 <nhat499@uw.edu>
2021-11-13 13:12:09 -0800

    maze and room class, we should have everything we need to make a maze in (main?) now

--- src/Model/Maze.java
+++ src/Model/Maze.java
@@ -1,5 +1,78 @@
 package Model;
 
-public class Maze {
+import java.util.Arrays;
 
+/**
+ * @author Nhat
+ * @version 1.0
+ * This class create a maze for the trivia maze
+ */
+
+public class Maze {
+    private int myLength;
+    private int myWidth;
+    private int myX = 0;
+    private int myY = 0;
+    private Room[][] myMaze;
+
+    // constructor for the maze
+    public Maze(int theLength, int theWidth){
+        myMaze = new Room[theLength][theWidth];
+    }
+
+    // add room to the maze
+    public void addRoom(Room theRoom) {
+        myX++;
+        if (myX > myLength) {
+            myX = 0;
+            myY++;
+        }
+        myY++;
+        if (myY <= myWidth) {
+            myMaze[myX][myY] = theRoom;
+        } else {
+            System.out.println("the maze is full");
+        }
+    }
+
+    /**
+     * get a room from the maze
+     * @param theX the x position of the room
+     * @param theY the y position of the room
+     * @return the room if exist or null
+     */
+    public Room getRoom(int theX, int theY) {
+        if (theX > myWidth || theY > myLength) {
+            System.out.println("There are no room at that index");
+            return null;
+        } else {
+            return myMaze[theX][theY];
+        }
+    }
+
+    // set and get size of the maze
+    public void setMyLength(int theLength) {
+        myLength = theLength;
+    }
+    public int getMyLength() {
+        return myLength;
+    }
+    public int getMyWidth() {
+        return myWidth;
+    }
+    public void setMyWidth(int theWidth) {
+        myWidth = theWidth;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < myLength; i++) {
+            for (int j = 0; j < myWidth; i++) {
+                s+= "#";
+            }
+            s+="\n";
+        }
+        return s;
+    }
 }
--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,5 +1,97 @@
 package Model;
 
+/**
+ * @author Nhat
+ * @version 1.0
+ * This class creates the room objects and each room
+ * hold 4 doors. The Room also will specify if it is an exit room
+ */
 public class Room {
+    private Door myNorthDoor;
+    private Door mySouthDoor;
+    private Door myWestDoor;
+    private Door myEastDoor;
+    private boolean myExit;
 
+    // constructor of the room class
+    public Room(Door theNorthDoor, Door theEastDoor, Door theSouthDoor, Door theWestDoor) {
+        myNorthDoor = theNorthDoor;
+        myEastDoor = theEastDoor;
+        mySouthDoor = theSouthDoor;
+        myWestDoor = theWestDoor;
+    };
+
+    /**
+     * setter for the exit room
+     * @param theExit if the room is an exit or not
+     */
+    public void setExit(boolean theExit) {
+        myExit = theExit;
+    }
+
+    /**
+     * getter for myExit
+     * @return if the room is an exit room
+     */
+    public boolean isExit() {
+        return myExit;
+    }
+
+    /**
+     * get the east door
+     * @return the east door
+     */
+    public Door getMyEastDoor() {
+        return myEastDoor;
+    }
+    /**
+     * get the north door
+     * @return the north door
+     */
+    public Door getMyNorthDoor() {
+        return myNorthDoor;
+    }
+    /**
+     * get the south door
+     * @return the south door
+     */
+    public Door getMySouthDoor() {
+        return mySouthDoor;
+    }
+    /**
+     * get the west door
+     * @return the west door
+     */
+    public Door getMyWestDoor() {
+        return myWestDoor;
+    }
+
+    /**
+     * set the east door
+     * @param theEastDoor the door to be the east door
+     */
+    public void setMyEastDoor(Door theEastDoor) {
+        myEastDoor = theEastDoor;
+    }
+    /**
+     * set the north door
+     * @param theNorthDoor the door to be the north door
+     */
+    public void setMyNorthDoor(Door theNorthDoor) {
+        myNorthDoor = theNorthDoor;
+    }
+    /**
+     * set the south door
+     * @param theSouthDoor the door to be the south door
+     */
+    public void setMySouthDoor(Door theSouthDoor) {
+        mySouthDoor = theSouthDoor;
+    }
+    /**
+     * set the west door
+     * @param theWestDoor the door to be the west door
+     */
+    public void setMyWestDoor(Door theWestDoor) {
+        myWestDoor = theWestDoor;
+    }
 }
