107e2dc3c15cba0c54e7c8d5fcc00706b1018ad4
Dylan <dylanchurchward710@gmail.com>
2021-11-14 17:29:55 -0800

    Updated door class to include question data

--- src/Model/Door.java
+++ src/Model/Door.java
@@ -1,68 +1,99 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
- * @author Andrew
- * @version 1.0
- * This class creates the door objects and has boolean values for whether the
- * door is closed or open.
+ * @author Andrew & Dylan
+ * @version 2.0
+ * This class creates the door objects and has boolean values
+ * for whether the door is closed or open.
  */
 public class Door {
 
     /**
-     * Boolean that opens the doors.
+     * Indicates whether this door is open or closed.
      */
-    public boolean myOpenStatus = true;
+    private boolean myOpenStatus;
 
     /**
-     * Boolean that closes the doors
+     * Indicates whether this door is locked or unlocked.
      */
-     public boolean myLockedStatus = false;
+     private boolean myLockedStatus;
 
     /**
-     * Constructor that lets us create the door objects.
+     * Correct answer for the question associated with this door.
      */
-    public Door() {
+    private final String myCorrectAnswer;
 
-    }
+    /**
+     * Collection of correct and incorrect answers for the question
+     * associated with this door.
+     */
+    private ArrayList<String> myAnswers;
+
+    /**
+     * The file path to the Pokemon image associated with the question
+     * of this door.
+     */
+    private final String myImgFilePath;
 
     /**
      * Parameterized Constructor that lets us create the door objects.
      */
-    public Door(final boolean theOpenStatus, final boolean theLockedStatus) {
-        myLockedStatus = theLockedStatus;
-        myOpenStatus = theOpenStatus;
+    public Door(final String theCorrectAnswer, final String theWrongAnswer1,
+                final String theWrongAnswer2, final String theWrongAnswer3,
+                final String theImgFilePath) {
+        myLockedStatus = false;
+        myOpenStatus = false;
+        myCorrectAnswer = theCorrectAnswer;
+        myAnswers.add(theCorrectAnswer);
+        myAnswers.add(theWrongAnswer1);
+        myAnswers.add(theWrongAnswer2);
+        myAnswers.add(theWrongAnswer3);
+        myImgFilePath = theImgFilePath;
+    }
+
+    /**
+     * To be called when the player attempts to pass through a closed doorway. If the player
+     * provides the correct answer to the question, the door is opened and remains
+     * unlocked. If the player answers the question incorrectly, the door remains closed
+     * and becomes locked and unpassable for the remainder of the game.
+     * @param thePlayersAnswer
+     */
+    public void attemptToOpen(final String thePlayersAnswer) {
+        if (thePlayersAnswer.equals(myCorrectAnswer)) {
+            myOpenStatus = true;
+        } else {
+            myLockedStatus = true;
+        }
+    }
+
+    /**
+     * Produces an ArrayList of the possible answers to the question associated
+     * with this door. The ArrayList is shuffled, meaning that while it includes
+     * the correct answer and all the possible wrong answers, they will appear
+     * in random order.
+     * @return ArrayList of all answers to the question associated with this door.
+     */
+    public ArrayList<String> getMyAnswers() {
+        Collections.shuffle(myAnswers);
+        return myAnswers;
     }
 
     /**
      * Getter for myOpenStatus.
      * @return myOpenStatus
      */
-    public boolean isMyOpenStatus() {
+    public boolean getMyOpenStatus() {
         return myOpenStatus;
-    }
-
-    /**
-     * Setter for myOpenStatus.
-     * @param theOpenStatus
-     */
-    public void setMyOpenStatus(boolean theOpenStatus) {
-        this.myOpenStatus = theOpenStatus;
     }
 
     /**
      * Getter for myLockedStatus.
      * @return myLockedStatus
      */
-    public boolean isMyLockedStatus() {
+    public boolean getMyLockedStatus() {
         return myLockedStatus;
     }
-
-    /**
-     * Setter for myLockedStatus
-     * @param theLockedStatus
-     */
-    public void setMyLockedStatus(boolean theLockedStatus) {
-        this.myLockedStatus = theLockedStatus;
-    }
-
 }
