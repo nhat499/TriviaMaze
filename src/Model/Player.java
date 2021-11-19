0d0aa7e8bc82084dad476fbc06e5d4548c345089
Dylan <dylanchurchward710@gmail.com>
2021-11-19 15:24:30 -0800

    Cleaned up javadocs and removed extraneous code in Room class. Removed Question class, we weren't using it.

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -2,9 +2,9 @@ package Model;
 
 /**
  * @author Andrew and Dylan
- * @version 1.0
- * This is the player class which dictates the position of a player object
- * and gives the player the ability to move to different rooms based on their current position.
+ * @version 1.0 - 11/19/2021
+ * The main function of the player class is keep tracker of the user's location
+ * and movement throughout their traversal of the TriviaMaze.
  */
 public class Player {
 
@@ -26,18 +26,11 @@ public class Player {
     /**
      * The position of the player.
      */
-//    private Room myPosition = myMaze.getRoom(myX, myY);
 
     /**
-     * Parameterless constructor.
-     */
-    public Player () {
-
-    }
-
-    /**
-     * Parameterized constructor that sets the players position.
-     * @param theX, theY
+     * Parameterized constructor for Player. Sets the players position
+     * in the maze.
+     * @param - theX, theY
      */
     public Player(final int theX, final int theY, final Maze theMaze) {
         myX = theX;
@@ -46,58 +39,54 @@ public class Player {
     }
 
     /**
-     * Method that moves the player north.
+     * Move the position of the player one index north, if possible.
      */
     public void moveNorth() {
         if (myY - 1 > 0) {
             myY = myY - 1;
         }
-
     }
-
-    public int getMyX() {
-        return myX;
-    }
-
-    public int getMyY() {
-        return myY;
-    }
-
 
     /**
-     * Method that moves the player south.
+     * Move the position of the player one index south, if possible.
      */
     public void moveSouth() {
         if(myY + 1 < myMaze.getMyWidth()) {
             myY = myY + 1;
         }
-
     }
 
     /**
-     * Method that moves the player west.
+     * Move the position of the player one index west, if possible.
      */
     public void moveWest() {
         if (myX - 1 > 0) {
             myX = myX - 1;
         }
-      ;
     }
 
     /**
-     * Method that moves the player east.
+     * Move the position of the player one index east, if possible.
      */
     public void moveEast() {
         if (myX + 1 < myMaze.getMyHeight()) {
             myX = myX + 1;
         }
-
     }
 
     /**
-     * A method that updates the room once the position is changed.
+     * Getter for myX.
+     * @return
      */
-//    private void updateRoom() {
-//        myPosition = myMaze.getRoom(myX, myY);
-//    }
+    public int getMyX() {
+        return myX;
+    }
+
+    /**
+     * Getter for myY.
+     * @return
+     */
+    public int getMyY() {
+        return myY;
+    }
 }
